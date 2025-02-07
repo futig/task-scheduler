4e57cbb93e3d2bb5492ed4067d0b4f1bce61389f
futig <everymusic8@gmail.com>
2025-02-07 16:11:58 +0500

    fix app

--- internal/app/app.go
+++ internal/app/app.go
@@ -18,46 +18,13 @@ var (
 	mu             sync.Mutex
 )
 
-func Run(token string) {func Run(token string) {
+func Run(token string) {
 	if token == "" {
 		log.Fatal("wrong token")
 	}
 
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		log.Fatal(fmt.Errorf("could not create connection with your token: %w", err))
-	}
-
-	updateConfig := tgbotapi.NewUpdate(0)
-	updateConfig.Timeout = 30
-	updates := bot.GetUpdatesChan(updateConfig)
-
-	for i := 1; i <= currentWorkers; i++ {
-		go worker(bot, i)
-	}
-
-	go manager(bot)
-
-	log.Printf("Бот: %s запущен с %d воркерами (мин=%d, макс=%d)",
-		bot.Self.UserName, currentWorkers, cfg.MinWorkers, cfg.MaxWorkers)
-
-	for update := range updates {
-		if update.Message != nil {
-			chatID := update.Message.Chat.ID
-
-			queueLen := len(tasksCh)
-			if queueLen > cfg.BusyThreshold {
-				interimMsg := tgbotapi.NewMessage(chatID, "Запрос обрабатывается. Пожалуйста, подождите результата.")
-				bot.Send(interimMsg)
-			}
-			tasksCh <- update
-		}
-	}
-
-	close(tasksCh)
-	close(stopWorkerCh)
-}
-
 		log.Fatal(fmt.Errorf("could not create connection with your token: %w", err))
 	}
 
