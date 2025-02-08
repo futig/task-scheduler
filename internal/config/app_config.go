43f5091f83730db58c117e159a4fc6cb81431d0a
futig <everymusic8@gmail.com>
2025-02-08 13:36:50 +0500

    config

--- internal/config/app_config.go
+++ internal/config/app_config.go
@@ -5,26 +5,60 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync"
+	"time"
+
+	"github.com/futig/task-scheduler/internal/domain"
+	"github.com/futig/task-scheduler/internal/storage"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type AppConfig struct {
-	MinWorkers         int
-	MaxWorkers         int
-	BusyThreshold      int
-	QueueSize          int
-	ScaleUpThreshold   int
-	ScaleDownThreshold int
+	MinWorkers           int
+	MaxWorkers           int
+	WorkersCheckInterval time.Duration
+	BusyThreshold        int
+	UpdatesQueueSize     int
+	RemindsQueueSize     int
+	ScaleUpThreshold     int
+	ScaleDownThreshold   int
+	RemindsCheckInterval        time.Duration
 }
 
-func NewAppConfig() AppConfig{
+func NewAppConfig() AppConfig {
 	return AppConfig{
-		MinWorkers: getIntVar("MIN_WORKERS"),
-		MaxWorkers: getIntVar("MAX_WORKERS"),
-		BusyThreshold: getIntVar("BUSY_THRESHOLD"),
-		QueueSize: getIntVar("QUEUE_SIZE"),
-		ScaleUpThreshold: getIntVar("SCALE_UP_THRESHOLD"),
-		ScaleDownThreshold: getIntVar("SCALE_DOWN_THRESHOLD"),
+		MinWorkers:           getIntVar("MIN_WORKERS"),
+		MaxWorkers:           getIntVar("MAX_WORKERS"),
+		WorkersCheckInterval: time.Duration(getIntVar("WORKERS_CHECK_INTERVAL")) * time.Second,
+		BusyThreshold:        getIntVar("BUSY_THRESHOLD"),
+		UpdatesQueueSize:     getIntVar("UPDATES_QUEUE_SIZE"),
+		RemindsQueueSize:     getIntVar("REMINDS_QUEUE_SIZE"),
+		ScaleUpThreshold:     getIntVar("SCALE_UP_THRESHOLD"),
+		ScaleDownThreshold:   getIntVar("SCALE_DOWN_THRESHOLD"),
+		RemindsCheckInterval:        time.Duration(getIntVar("CHECK_REMINDS_INTERVAL")) * time.Minute,
 	}
+}
+
+type WorkflowConfig struct {
+	RemindsCh    chan domain.Task
+	UpdatesCh    chan tgbotapi.Update
+	StopWorkerCh chan struct{}
+	Mu           sync.Mutex
+	Storage      storage.Storage
+}
+
+func NewWorkflowConfig() WorkflowConfig {
+	return WorkflowConfig{
+		RemindsCh:    make(chan domain.Task, getIntVar("REMINDS_QUEUE_SIZE")),
+		UpdatesCh:    make(chan tgbotapi.Update, getIntVar("UPDATES_QUEUE_SIZE")),
+		StopWorkerCh: make(chan struct{}),
+	}
+}
+
+func (w *WorkflowConfig) CloseCh() {
+	close(w.RemindsCh)
+	close(w.UpdatesCh)
+	close(w.StopWorkerCh)
 }
 
 func getIntVar(key string) int {
