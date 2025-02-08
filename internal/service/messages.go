db7507fd4f9699b84cb9a1fc1165115b73a7229f
futig <everymusic8@gmail.com>
2025-02-08 13:51:02 +0500

    todo

--- internal/service/messages.go
+++ internal/service/messages.go
@@ -5,6 +5,7 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+// TODO: вынести метод в app, сделать сервисы независимыми от телеграма
 func SendMessage(bot *tgbotapi.BotAPI, chatID int64, message string) (err error) {	
 	interimMsg := tgbotapi.NewMessage(chatID, message)
 	_, err = bot.Send(interimMsg)
